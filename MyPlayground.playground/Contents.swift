77963677d7cdf223cc75c5e305a5bd0761d6b6db
Guilherme Enes <guilherme.epecci@gmail.com>
2020-04-27 18:00:42 -0300

    Feito

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -18,6 +18,60 @@ pilha.push(intA)
 pilha.push(intB)
 
 //TODO: Fazer um método que recebe um Array e um elemento, retorna um Bool dizendo se o elemento se encontra no Array
+
+
+
+public func elementExist<Element: Comparable>(_ array: [Element], _ element: Element) -> Bool{
+    for index in array {
+        if index == element {
+            return true
+        }
+    }
+     return false
+}
+
+
 //TODO: Extensão de Array contendo uma função que retorna a soma de todos os elementos, independente de qual o valor numérico
+extension Array where Element: Numeric {
+    func sum() -> Element {
+        var soma = Element.zero
+        for element in self {
+            soma += element
+        }
+        return soma
+    }
+}
 //TODO: Fazer um método que recebe 2 Arrays e retorna um Int referente a quantidade de elementos presentes em ambos os Arrays
+public func recebeDoisArrays<T: Equatable>(array1: [T], array2: [T]) -> [T] {
+    var interseccao: [T] = array1 + array2
+    
+    for i in array1.indices {
+        for z in array2 {
+            if array1[i] == z {
+                interseccao.remove(at: i)
+            }
+        }
+    }
+    return interseccao
+}
+
+
     //BONUS TODO: Fazer o método acima receber N arrays
+
+    func commonElements<T: Equatable>(arrays: Array<T>...) -> [T]? {
+        var copy = arrays
+        let firstArray = copy.removeFirst()
+        var result = [T]()
+
+        for element in firstArray {
+            for array in copy {
+                if !array.contains(element){
+                    break
+                }
+                if array == arrays.last{
+                    result.append(element)
+                }
+            }
+        }
+        return result
+    }
